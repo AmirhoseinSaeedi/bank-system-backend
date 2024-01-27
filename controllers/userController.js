fb5ed914c4962089a2f7c5352b7379f95c507d56
mahaamd <mohammadamiri.ma98@gmail.com>
2024-01-27 22:13:44 +0330

    showing all customer page

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,17 +4,18 @@ const asyncHandler = require("express-async-handler");
 exports.getAll = asyncHandler(async (req, res, next) => {
   connection.query("SELECT * FROM User", (error, results, fields) => {
     if (error) throw error;
-    res.send(results);
+    res.render('customer_list',{results});
   });
 });
 
 exports.get = asyncHandler(async (req, res, next) => {
   const id = req.params.id;
+  console.log(id + "  helllllo")
   connection.query(
     `SELECT * FROM User Where id = ${id}`,
     (error, results, fields) => {
       if (error) throw error;
-      res.send(results);
+      res.render('customer_view',{results});
     }
   );
 });
