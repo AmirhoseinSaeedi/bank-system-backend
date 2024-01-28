ee9b7fe3b7acb358fe14fec034f48a11ed789a74
mahaamd <mohammadamiri.ma98@gmail.com>
2024-01-28 12:04:42 +0330

    completing customer view page

--- db/database.js
+++ db/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "54655465",
+  password: "mypassword",
   database: "bank",
 });
 
